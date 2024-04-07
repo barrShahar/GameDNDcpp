5f7d6ca7785419d4a24e556736ae193954867b55
barrShahar <59974036+barrShahar@users.noreply.github.com>
2024-04-07 19:29:55 +0300

    Update StringActionResponse.cpp

--- DND_staticLib/source/Action/StringActionResponse.cpp
+++ DND_staticLib/source/Action/StringActionResponse.cpp
@@ -7,14 +7,8 @@ experis::StringActionResponse::StringActionResponse(const std::string& a_reply)
 	: m_reply { std::make_unique<char[]>(a_reply.size() + 1) }
 	, m_dataSize { a_reply.size() }
 {
-	// Ctor
-	/*std::copy(a_reply.begin(), a_reply.end(), m_reply.get());*/
-	size_t i = 0;
-	for (; i < a_reply.size(); ++i)
-	{
-		m_reply[i] = a_reply[i];
-	}
-	m_reply[i] = '\0';
+	std::copy(a_reply.begin(), a_reply.end(), m_reply.get());
+	m_reply[m_dataSize] = '\0'; // Null-terminate the string
 }
 
 ReplyType experis::StringActionResponse::GetResponse()
